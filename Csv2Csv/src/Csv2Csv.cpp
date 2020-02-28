c36dac205693740048daf96576de0ca84b516acf
die-edda <Hi@die-edda.de>
2020-02-28 13:49:49 +0100

    Initial commit

---
+++ Csv2Csv/src/Csv2Csv.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Csv2Csv.cpp
+// Author      : Edda Nourbakhsh
+// Version     :
+// Copyright   : © Edda Nourbakhsh, 2020, all rights reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
